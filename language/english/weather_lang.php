37b76f60c5aa845d91e7c8aceb1b58fff9b80958
Adam Kelly <adam.guitar12@gmail.com>
2018-06-19 23:21:51 +0100

    Initial Commit

---
+++ language/english/weather_lang.php
@@ -0,0 +1,7 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang = array(
+);
+
+/* End of file weather_lang.php */
+/* Location: /system/expressionengine/third_party/weather/language/english/weather_lang.php */
